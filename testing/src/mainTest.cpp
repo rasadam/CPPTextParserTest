8748c2f6bdaeedf248cded6863cc667ac27beb0d
Adam Borenstein <adam@ras.land>
2021-09-14 11:52:59 +0300

    added printing to main test for next lines

--- testing/src/mainTest.cpp
+++ testing/src/mainTest.cpp
@@ -1,11 +1,12 @@
 #include <iFileReader.h>
 #include <UTF8Reader.h>
+#include <iostream>
 
 using namespace RAS;
-
+using namespace std;
 int main(int argc, char const *argv[])
 {
     UFT8Reader reader = UFT8Reader( "SomeFile.txt" );
-    reader.GetNextLine();
+    cout << reader.GetNextLine() << endl;
     return 0;
 }
