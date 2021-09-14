e66b47f1cd7284f7c093cbeceb1b8430287871bd
Adam Borenstein <adam@ras.land>
2021-09-14 16:32:07 +0300

    changed parsers container to be a map

--- testing/src/mainTest.cpp
+++ testing/src/mainTest.cpp
@@ -2,17 +2,22 @@
 #include <UTF8Reader.h>
 #include <UTF16Reader.h>
 #include <iostream>
+#include <map>
 
 using namespace RAS;
 using namespace std;
 int main(int argc, char const *argv[])
 {
     iFileReader* readers[2];
-    readers[0] = new UFT8Reader( "SomeFile.txt" );
-    readers[1] = new UTF16Reader( "SomeFile.txt" );
-    cout << readers[0]->GetNextLine() << endl;
-    cout << readers[0]->GetNextLine( 10 ) << endl;
-    cout << readers[1]->GetNextLine() << endl;
-    cout << readers[1]->GetNextLine( 10 ) << endl;
+    map<string, iFileReader*> parsers;
+    parsers[ "UTF8" ] = new UFT8Reader( "SomeFile.txt" );; 
+    parsers[ "UTF16" ] = new UTF16Reader( "SomeFile.txt" );; 
+
+    // readers[0] = new UFT8Reader( "SomeFile.txt" );
+    // readers[1] = new UTF16Reader( "SomeFile.txt" );
+    cout << parsers[ "UTF8" ]->GetNextLine() << endl;
+    cout << parsers[ "UTF8" ]->GetNextLine( 10 ) << endl;
+    cout << parsers[ "UTF16" ]->GetNextLine() << endl;
+    cout << parsers[ "UTF16" ]->GetNextLine( 10 ) << endl;
     return 0;
 }
