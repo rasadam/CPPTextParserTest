0cbdd81cb0824875f2bfc2960124bac00fe2822d
Adam Borenstein <adam@ras.land>
2021-09-14 11:26:42 +0300

    added get lines stub

--- src/UTF8Reader.cpp
+++ src/UTF8Reader.cpp
@@ -1,10 +1,22 @@
 #include <UTF8Reader.h>; 
 #include <iostream>;
+
+using namespace std; 
+
 namespace RAS
 {
 
-UFT8Reader::UFT8Reader ( std::string fileName ):iFileReader( fileName ){
-    std::cout << "Created UTF8Reader onject." << std::endl;
+UFT8Reader::UFT8Reader ( string fileName ):iFileReader( fileName ){
+    cout << "Created UTF8Reader onject." << endl;
+}
+
+UFT8Reader::~UFT8Reader() {
+    cout << "destructing UTF8Reader object." << endl;
+}
+
+string GetNextLine( int numLines = 1 ){
+    string lines =  "returning lines 1 - " + numLines;
+    return lines; 
 }
 
 }//end of namespace
--- src/UTF8Reader.h
+++ src/UTF8Reader.h
@@ -9,6 +9,8 @@ public:
      UFT8Reader ( std::string filename );
     ~UFT8Reader ();
 
+    std::string GetNextLine( int numLines = 1 );
+
 }; // end of class
 
 } //end of namespace
