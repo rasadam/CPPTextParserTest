1ea6d202cd6cd334dddd52f6de046696262d553e
Adam Borenstein <adam@ras.land>
2021-09-14 12:47:27 +0300

    changed test to wirk with array of base class pointers

--- testing/src/mainTest.cpp
+++ testing/src/mainTest.cpp
@@ -1,13 +1,18 @@
 #include <iFileReader.h>
 #include <UTF8Reader.h>
+#include <UTF16Reader.h>
 #include <iostream>
 
 using namespace RAS;
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    iFileReader*  reader = new UFT8Reader( "SomeFile.txt" );
-    cout << reader->GetNextLine() << endl;
-    cout << reader->GetNextLine( 10 ) << endl;
+    iFileReader* readers[2];
+    readers[0] = new UFT8Reader( "SomeFile.txt" );
+    readers[1] = new UTF16Reader( "SomeFile.txt" );
+    cout << readers[0]->GetNextLine() << endl;
+    cout << readers[0]->GetNextLine( 10 ) << endl;
+    cout << readers[1]->GetNextLine() << endl;
+    cout << readers[1]->GetNextLine( 10 ) << endl;
     return 0;
 }
